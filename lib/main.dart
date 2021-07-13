9349e07ca559ff72ea700c9aea73ef801ec222e2
roktim32 <roktimsenapoty2@gmail.com>
2021-07-13 23:23:53 +0530

    text

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Quiz App"),
+          title: Text("Quiz Master"),
         ),
         body: Center(
           child: Container(
